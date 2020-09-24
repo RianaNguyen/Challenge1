e5eb9383292709fe76df3295e1f0ef18352a14d1
RianaNguyen <71785609+RianaNguyen@users.noreply.github.com>
2020-09-24 14:05:54 -0400

    Add files via upload

---
+++ Challenge3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Challenge3 {
+	public static void main (String[]args){
+		Scanner scan = new Scanner(System.in);
+			System.out.println("Player 1 choose R, P or S");
+				String p1 = scan.nextLine();
+			System.out.println("Player 2 choose R, P or S");
+				String p2 = scan.nextLine();
+				
+				
+	}
+}
